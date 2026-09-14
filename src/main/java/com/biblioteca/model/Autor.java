58668b20dd3689e494cfb04886181174c12eb0f7
Mari Ramos Aguilar <rmia1929@gmail.com>
2026-09-13 22:11:28 -0500

    feat: crear modelos de usuario y  autor

---
+++ src/main/java/com/biblioteca/model/Autor.java
@@ -0,0 +1,34 @@
+package com.biblioteca.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "autores")
+public class Autor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idAutor;
+
+    @Column(nullable = false, length = 100)
+    private String nombre;
+
+    public Autor() {
+    }
+
+    public Long getIdAutor() {
+        return idAutor;
+    }
+
+    public void setIdAutor(Long idAutor) {
+        this.idAutor = idAutor;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+}
